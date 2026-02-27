210f2fe6e648c4ba2345895eb7ee0537b7578f9f
Brady Welsh <blwelsh@calpoly.edu>
2026-02-26 21:54:59 -0800

    beginning parts of activity card functionality

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -4,11 +4,14 @@ type ActivityCardProps = {
   title: string;
   value: string;
   type?: 'default' | 'new-events' | 'warning' | 'high-risk' | 'cyberbullying';
+  onClick: React.MouseEventHandler<HTMLDivElement>;
+  viewed: boolean
 };
 
-export default function ActivityCard({ title, value, type = 'default' }: ActivityCardProps) {
+export default function ActivityCard({ title, value, type = 'default', onClick, viewed }: ActivityCardProps) {
+
   return (
-    <div className={`activity-card activity-card--${type}`}>
+    <div className={`${viewed ? "modified-activity-card activity-card" : "activity-card"} activity-card--${type}`} onClick={onClick} >
       <div className="activity-value">{value}</div>
       <div className="activity-title">{title}</div>
     </div>
--- src/components/TodayActivity.tsx
+++ src/components/TodayActivity.tsx
@@ -1,15 +1,22 @@
 import "../styles/Dashboard.css";
 import ActivityCard from "./ActivityCard";
+import { useState } from "react";
 
 export default function TodayActivity() {
+    const [viewingNewEvents, setViewingNewEvents] = useState<boolean>(false);
+    const [viewingWarnings, setViewingWarnings] = useState<boolean>(false);
+    const [viewingHighRisk, setViewingHighRisk] = useState<boolean>(false);
+    const [viewingCyber, setViewingCyber] = useState<boolean>(false);
+
   return (
     <div className="section">
       <h2 className="section-title">Today's Activity</h2>
-      <div className="activity-grid">
-        <ActivityCard title="New Events" value="3" type="new-events" />
-        <ActivityCard title="Warnings" value="1" type="warning" />
-        <ActivityCard title="High-Risk" value="1" type="high-risk" />
-        <ActivityCard title="Cyberbullying" value="1" type="cyberbullying" />
+      <div className={(viewingNewEvents || viewingWarnings || viewingHighRisk || viewingCyber ) ? "modified-activity-grid" : "activity-grid"}>
+          
+          {!viewingWarnings && !viewingHighRisk && !viewingCyber && <ActivityCard title="New Events" value="3" type="new-events" viewed={viewingNewEvents} onClick={() => setViewingNewEvents(!viewingNewEvents)}/>}
+          {!viewingNewEvents && !viewingHighRisk && !viewingCyber && <ActivityCard title="Warnings" value="1" type="warning" viewed={viewingWarnings} onClick={() => setViewingWarnings(!viewingWarnings)}/>}
+          {!viewingWarnings && !viewingNewEvents && !viewingCyber && <ActivityCard title="High-Risk" value="1" type="high-risk" viewed={viewingHighRisk} onClick={() => setViewingHighRisk(!viewingHighRisk)}/>}
+          {!viewingWarnings && !viewingHighRisk && !viewingNewEvents && <ActivityCard title="Cyberbullying" value="1" type="cyberbullying" viewed={viewingCyber} onClick={() => setViewingCyber(!viewingCyber)}/>}
       </div>
     </div>
   );
